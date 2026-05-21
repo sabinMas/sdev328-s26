c0f547d6e5f678877e20449ce89bcc5b2e9deb08
Josh Archer <archerjoshb@gmail.com>
2026-05-21 13:24:07 -0700

    Added update to the api

--- monuments-api/src/controllers/monuments.controller.js
+++ monuments-api/src/controllers/monuments.controller.js
@@ -1,4 +1,4 @@
-import { allMonuments, addMonument } from '../services/monuments.service.js';
+import { allMonuments, addMonument, updateMonument } from '../services/monuments.service.js';
 
 export const getMons = (req, res) => {
     const monArray = allMonuments();
@@ -13,6 +13,26 @@ export const addMon = (req, res) => {
         res.status(201).json({
             message: "Monument added successfully"
         })
+    } else {
+        res.status(400).json({
+            message: "Missing monument"
+        })
+    }
+}
+
+export const updateMon = (req, res) => {
+    const mon = req.body;
+    if (mon) {
+        const result = updateMonument(mon);
+        if (result) {
+            res.status(200).json({
+                message: "Monument updated"
+            })
+        } else {
+            res.status(404).json({
+                message: "Monument not found"
+            })
+        }
     } else {
         res.status(400).json({
             message: "Missing monument"
