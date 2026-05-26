6f94b9fd12eabf19bafabf545c4a6fd6cc37a037
Josh Archer <archerjoshb@gmail.com>
2026-05-26 14:53:13 -0700

    Adding an example of cookies.

---
+++ web-app-state/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import path from "path";
+
+import indexRoutes from "./routers/index.routes.js";
+import stateRoutes from "./routers/state.routes.js";
+import multiFormRoutes from "./routers/form.routes.js";
+import statusRoutes from "./routers/status.routes.js";
+import cookieParser from "cookie-parser";
+import { visitorCount } from "./middleware/middleware.js";
+
+const app = express();
+
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "src", "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(visitorCount);
+
+app.use(indexRoutes);
+app.use(stateRoutes);
+app.use(multiFormRoutes);
+app.use(statusRoutes);
+
+export default app;
